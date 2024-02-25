ccb5bdb3f887f507058f5c34ebf3e56c58ee8f23
Eduard-Polodeanu <polodeanu.eduard@gmail.com>
2024-02-25 18:31:59 +0200

    Add collisions

--- main.py
+++ main.py
@@ -6,29 +6,30 @@ def scale_image(img, factor):
     size = round(img.get_width() * factor), round(img.get_height() * factor)
     return pygame.transform.scale(img, size)
 
-
+# draw the image based on the rotation angle of the car
 def blit_rotate_center(win, image, top_left, angle):
     rotated_image = pygame.transform.rotate(image, angle)
-    new_rect = rotated_image.get_rect(
-        center=image.get_rect(topleft=top_left).center)
+    new_rect = rotated_image.get_rect(center=image.get_rect(topleft=top_left).center)
     win.blit(rotated_image, new_rect.topleft)
 
 
-TRACK = scale_image(pygame.image.load("assets/track.png"), 1)
-TRACK_BORDER = scale_image(pygame.image.load("assets/track-border.png"), 1)
+TRACK = pygame.image.load("assets/track.png")
+TRACK_BORDER = pygame.image.load("assets/track-hitbox.png")
+TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
 
 CAR = scale_image(pygame.image.load("assets/car.png"), 0.5)
+CAR_MASK = pygame.mask.from_surface(scale_image(pygame.image.load("assets/car-hitbox.png"), 0.4))
 
 WIN_SIZE = (1280, 720)
-WIN = pygame.display.set_mode(WIN_SIZE)
+WIN = pygame.display.set_mode(WIN_SIZE, flags=pygame.SCALED, vsync=1)
 
 pygame.display.set_caption("Racing Game!")
 
-FPS = 60
-START_POS = (180, 70)
+FPS = 30
+START_POS = (400, 64)
 
-MAX_VELOCITY = 3
-ROTATION_VELOCITY = 2.2
+MAX_VELOCITY = 4
+ROTATION_VELOCITY = 4
 ACCELERATION = 0.1
 
 
@@ -71,6 +72,17 @@ class Car:
         self.vel = max(self.vel - self.acceleration / 2, 0)
         self.move()
 
+    def collide(self, mask, x=0, y=0):
+        offset = (int(self.x - x), int(self.y - y))
+        intersection_point = mask.overlap(CAR_MASK, offset)
+        print(intersection_point)
+        return intersection_point
+    
+    def hit_wall(self):
+        self.vel = -self.vel/2
+        self.move()
+        
+
 
 def draw(win, images, car):
     for img, pos in images:
@@ -86,7 +98,7 @@ images = [(TRACK, (0, 0)), (TRACK_BORDER, (0, 0))]
 car = Car(MAX_VELOCITY, ROTATION_VELOCITY, ACCELERATION)
 
 while running:
-    clock.tick(FPS)
+    clock.tick(FPS) # clock.tick_busy_loop(FPS)
 
     draw(WIN, images, car)
 
@@ -96,21 +108,25 @@ while running:
             break
 
     keys = pygame.key.get_pressed()
-    moved = False
+    moving = False
 
     if keys[pygame.K_a]:
         car.rotate(left=True)
     if keys[pygame.K_d]:
         car.rotate(right=True)
     if keys[pygame.K_w]:
-        moved = True
+        moving = True
         car.move_forward()
     if keys[pygame.K_s]:
-        moved = True
+        moving = True
         car.move_backward()
 
-    if not moved:
+    if not moving:
         car.reduce_speed()
 
+
+    if car.collide(TRACK_BORDER_MASK) != None:
+        car.hit_wall()
+    
 
 pygame.quit()
